3aa663b73f73644609b9a6632d9fb66232fca74e
UROP01 <pyzhang086@gmail.com>
2025-02-25 01:44:45 -0800

    lower binary operator

--- src/ModifyAST.cpp
+++ src/ModifyAST.cpp
@@ -6,6 +6,8 @@
 #include "clang/Frontend/FrontendAction.h"
 #include "llvm/Support/raw_ostream.h"
 #include <memory>
+#include <ctime>
+static std::atomic<int> variableCounter(0);
 
 using namespace clang::tooling;
 using namespace llvm;
@@ -14,25 +16,47 @@ using namespace std;
 
 namespace ModifyAST {
 
-
-
 class MyASTVisitor : public RecursiveASTVisitor<MyASTVisitor> {
 
 public:
-    explicit MyASTVisitor(Rewriter &R) : rewriter(R) {}
+    explicit MyASTVisitor(Rewriter &R) : rewriter(R) {
+        srand(static_cast<unsigned int>(time(0)));
+    }
 
     //if visit a expression, print it
     bool VisitExpr(Expr *E) {
-        if (E->getStmtClassName() == "BinaryOperator") {
-            BinaryOperator *B = cast<BinaryOperator>(E);
-            SourceLocation ST = B->getOperatorLoc();
-            rewriter.InsertText(ST, "/*This is a binary operator*/", true, true);
-        }
-        return true;
+        // if (E->getStmtClassName() == "BinaryOperator") {
+        //     BinaryOperator *B = cast<BinaryOperator>(E);
+        //     SourceLocation ST = B->getOperatorLoc();
+        //     rewriter.InsertText(ST, "/*This is a binary operator*/", true, true);
+        // }
+        
+        return LowerExpr(E);
     }
 
 private:
     Rewriter &rewriter;
+
+    bool LowerExpr(Expr *E) {
+        //if(rand() % 2 == 0) return true;
+        if(E->getStmtClassName() == "BinaryOperator") {
+            BinaryOperator *B = cast<BinaryOperator>(E);
+            return LowerBinaryOperator(B);
+        }
+        return true;
+    }
+
+    bool LowerBinaryOperator(BinaryOperator *B) {
+        Expr *LHS = B->getLHS();
+        Expr *RHS = B->getRHS();
+        LowerExpr(LHS);
+        LowerExpr(RHS);
+        //create a variable name that does not exist in the code
+        string varName = "auto _myvar" + to_string(variableCounter++);
+        //create a new variable declaration
+        rewriter.InsertText(B->getOperatorLoc(), varName + " = " + LHS->getSourceRange().getBegin().printToString(rewriter.getSourceMgr()) + " " + B->getOpcodeStr().str() + " " + RHS->getSourceRange().getBegin().printToString(rewriter.getSourceMgr()), true, true);
+        return true;
+    }
 };
 
 class MyASTConsumer : public ASTConsumer {
